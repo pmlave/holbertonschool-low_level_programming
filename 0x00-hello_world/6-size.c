9fc8efb651a179e1eefed1c40e92cbb02ad0aecf
pmlave <pmlave@gmail.com>
2018-06-20 21:24:21 +0000

    Size update again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("\nsize of a char: %d", sizeof(char *));
-	printf("\nsize of an int: %d", sizeof(int *));
-	printf("\nsize of a long int: %d", sizeof(long int *));
-	printf("\nsize of a long long int %d", sizeof(long long *));
-	printf("\nsize of a float: %d", sizeof(float *));
+	printf("\nsize of a char: %d", sizeof(char));
+	printf("\nsize of an int: %d", sizeof(int));
+	printf("\nsize of a long int: %d", sizeof(long int));
+	printf("\nsize of a long long int: %d", sizeof(long long));
+	printf("\nsize of a float: %d\n", sizeof(float));
 	return (0);
 }

b21d9f509f8050afd825bd1e8422deadc774704f
pmlave <pmlave@gmail.com>
2018-06-20 20:42:23 +0000

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- *main - test format
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-/**
-*puts - prints the text
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

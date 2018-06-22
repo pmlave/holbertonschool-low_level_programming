2a76a1a0521dcbf592bfbccc6acce67937db6a99
pmlave <pmlave@gmail.com>
2018-06-22 21:13:10 +0000

    Triple number skipping

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+				{
+			putchar(a);
+			putchar(b);
+			putchar(c);
+			if (a != '7' || b != '8' || c != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+				}
+}
+	}
+	putchar('\n');
+	return (0);
+}

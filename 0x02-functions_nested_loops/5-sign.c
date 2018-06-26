c2ac980df3efd4b8f8c8ab21225b6f6f182d8500
pmlave <pmlave@gmail.com>
2018-06-26 19:36:04 +0000

    Adding 5 and updating header

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *print_sign - function for determining sign of character
+ *@n: passed in character
+ *Return: 1 for positive, 0 for 0, -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+}

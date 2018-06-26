2ed0e3f7193a593f8a3e7365aba303aa125e2d81
pmlave <pmlave@gmail.com>
2018-06-26 18:58:39 +0000

    Adding my holberton printing function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
+/**
+ *main - entry point
+ *Return: 0 always
+ */
 
 int main(void)
 {
 	char school[] = "Holberton";
 	int c;
 
-	for (c = 0; c <= 10; c++)
+	for (c = 0; c < 10; c++)
 	{
 		_putchar(school[c]);
 	}
+	_putchar('\n');
 	return (0);
 }

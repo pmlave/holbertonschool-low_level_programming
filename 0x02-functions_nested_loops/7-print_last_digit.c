1b900fc18e23acdc3fa8d52b85f1b14344d544ee
pmlave <pmlave@gmail.com>
2018-06-26 20:59:24 +0000

    Updated source file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,10 @@ int print_last_digit(int n)
 		_putchar('0' + (n % 10));
 		return (n % 10);
 	}
-	else if (n <= 0)
+	else
 	{
 		n = (n * -1);
 		_putchar('0' + (n % 10));
 		return (n % 10);
 	}
-	return (0);
 }

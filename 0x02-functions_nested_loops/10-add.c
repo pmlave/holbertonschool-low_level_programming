6020e9168c59ed1762a4d24ea02e08e9304314a7
pmlave <pmlave@gmail.com>
2018-06-27 00:23:02 +0000

    Adding new add function and new header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *add - combine integers
+ *@a: two passed in integers
+ *@b: second integer passed in
+ *Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

adc3022ad3a59eccaa335f4a5e8fccbc538304a0
pmlave <pmlave@gmail.com>
2018-06-27 02:45:17 +0000

    Holberton print update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,15 @@
 
 int main(void)
 {
-	char school[] = "Holberton";
-	int c;
-
-	for (c = 0; c < 10; c++)
-	{
-		_putchar(school[c]);
-	}
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
 	_putchar('\n');
 	return (0);
 }

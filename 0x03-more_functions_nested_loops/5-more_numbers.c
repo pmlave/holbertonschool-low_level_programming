8a4a78216be702399a0faf762988c6d1a81a2f9a
pmlave <pmlave@gmail.com>
2018-06-28 21:27:59 +0000

    New header file and printing program

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *more_numbers - printing numbers many times
+ *
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int a, i;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (a = 00; a <= 14; a++)
+		{
+			if (a > 9)
+			_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+			}
+	_putchar('\n');
+
+	}
+
+}

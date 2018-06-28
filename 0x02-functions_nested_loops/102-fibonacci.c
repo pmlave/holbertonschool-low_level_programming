be3045d0f3c9f548f407a0689a482963e5a358b6
pmlave <pmlave@gmail.com>
2018-06-28 00:36:35 +0000

    Fibonacci update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: Getting fibonacci numbers
+ *Return: 0
+ */
+int main(void)
+{
+	int long long x, y, z, a;
+
+	x = 1;
+	y = 2;
+	for (a = 0; a < 50; a++)
+	{
+		z = x + y;
+		if (z < 0)
+			z = z * -1;
+		printf("%lli", x);
+		if (a < 49)
+			printf(", ");
+		x = y;
+		y = z;
+	}
+	printf("\n");
+	return (0);
+
+}

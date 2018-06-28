e1849200a0bc0d52bf1af99ea700fb9e647c7752
pmlave <pmlave@gmail.com>
2018-06-28 19:46:13 +0000

    New commit for printing program and header file update

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - printing 0-9
+ *
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}

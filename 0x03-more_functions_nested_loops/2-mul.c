6d08f64cea9e78f9176883dc14734a0bc0e3e58a
pmlave <pmlave@gmail.com>
2018-06-28 19:32:33 +0000

    New multiplication program and header file update:

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *mul - multiplying two given numbers
+ *@a:first character
+ *@b: second character
+ *Return: the product value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

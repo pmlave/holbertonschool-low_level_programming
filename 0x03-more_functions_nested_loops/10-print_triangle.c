c924d66288588d9fbfb4e8a9fb51a42caeda93bd
pmlave <pmlave@gmail.com>
2018-06-29 00:01:04 +0000

    New triangle printing program and updated header file

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *print_triangle - printing a triangle with # characters
+ *@size: passed in value for size of triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = a; b < size; b++)
+			{
+				_putchar(' ');
+			}
+			for (c = ((b - a) + 1); c <= size; c++)
+				_putchar('#');
+			_putchar('#');
+			_putchar('\n');
+			}
+		}
+
+	else
+		_putchar('\n');
+}

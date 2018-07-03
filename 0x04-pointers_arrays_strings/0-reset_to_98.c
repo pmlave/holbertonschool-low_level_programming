ae3d71a55ff1160a4eefdc48860c3d48bc68620b
pmlave <pmlave@gmail.com>
2018-07-03 19:03:05 +0000

    Initial commit of README, header file, and question 0

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - changing number with pointer
+ *@*n: initial number
+ *
+ *Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x04-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Working with pointers
+---
+1. Simple code updating the value of a pointer.
+2.

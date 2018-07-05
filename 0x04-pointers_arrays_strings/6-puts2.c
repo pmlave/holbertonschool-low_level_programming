a7083a839d2a25aeebcace742c04253d1a08ebfa
pmlave <pmlave@gmail.com>
2018-07-05 05:49:45 +0000

    Updating modulo value

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -14,12 +14,9 @@ void puts2(char *str)
 	a = 0;
 	while (str[a] != '\0')
 	{
-		if (str[a] != '\0')
+		if (a % 2 == 0 || a == 0)
 		{
-			if (str[a] % 2 == 0 || str[0])
-			{
-				_putchar(str[a]);
-			}
+			_putchar(str[a]);
 		}
 		a++;
 	}

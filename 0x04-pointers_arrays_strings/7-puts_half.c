6b00f2be394b3a0874a944e4d4e008653eddf4fc
pmlave <pmlave@gmail.com>
2018-07-06 05:11:05 +0000

    New question 7 and updated header file

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ *puts_half - printing half of the string
+ *@str: passed in string value
+ *Return: Void
+ */
+
+void puts_half(char *str)
+{
+	int a, b;
+
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+	if (a % 2 == 0)
+	{
+		b = a /2;
+		while (str[b] != '\0')
+		{
+			_putchar(str[b]);
+			b++;
+		}
+	}
+	else
+	{
+		b = (a / 2) + 1;
+		while (str[b] != '\0')
+		{
+			_putchar(str[b]);
+			b++;
+		}
+	}
+	_putchar('\n');
+}

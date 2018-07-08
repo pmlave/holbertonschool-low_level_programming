9b1638100cf5e7fff80cb81cc91ed66a2b89702b
pmlave <pmlave@gmail.com>
2018-07-08 00:31:27 +0000

    Updating code for #7

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,15 +10,15 @@ char *leet(char *s)
 {
 	char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0, k = 0; j < 10; j++, k++)
 		{
 			if (s[i] == a[j])
 			{
-				s[i] = '0' + b[j];
+				s[i] = '0' + b[k];
 			}
 		}
 	}

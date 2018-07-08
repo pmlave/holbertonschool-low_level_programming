c466f749aaef1e9faae2e9d5ab2fa242aac35ca9
pmlave <pmlave@gmail.com>
2018-07-08 00:35:47 +0000

    Updating matching code

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 
 char *leet(char *s)
 {
-	char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 	int i, j, k;
 
@@ -18,7 +18,7 @@ char *leet(char *s)
 		{
 			if (s[i] == a[j])
 			{
-				s[i] = '0' + b[k];
+				s[i] = b[k] + '0';
 			}
 		}
 	}

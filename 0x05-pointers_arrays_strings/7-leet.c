d3a648e0c5e0510e1d992feaf63c56b6424798f0
pmlave <pmlave@gmail.com>
2018-07-08 00:40:22 +0000

    Checking the checker

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -8,9 +8,12 @@
 
 char *leet(char *s)
 {
+	int i;
+	int j;
+	int k;
+
 	int a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-	int i, j, k;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{

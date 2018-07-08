c1cfabc7fc5a11745e8f44f3191371fd3b2baf1a
pmlave <pmlave@gmail.com>
2018-07-08 03:58:33 +0000

    Final attempt on number 7

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,25 @@
 #include "holberton.h"
 /**
- *leet - switching characters out with limited loops and conditionals
- *@s: given string
- *Description: See above
- *Return: pointer to string value
+ *leet - changing values to 1337
+ *@s: value of string
+ *Description: First line
+ *Return: pointer to string
  */
 char *leet(char *s)
 {
-	int i;
-	int j;
-	int k;
-
+	int i, j;
 	int a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0, k = 0; j < 10; j++, k++)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == a[j])
 			{
-				s[i] = b[k] + '0';
+				s[i] = b[j] + '0';
 			}
 		}
 	}
-
 	return (s);
 }

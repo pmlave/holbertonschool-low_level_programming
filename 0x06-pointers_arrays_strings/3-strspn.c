54277a2dd27d4067f5714cbec49cb1d3171bc318
pmlave <pmlave@gmail.com>
2018-07-11 01:27:05 +0000

    New header file and answer for #3

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *_strspan - gets the length of a prefix substring
+ *@s: initial segment
+ *@accept: original string
+ *Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a;
+	int b;
+	int c = 0;
+	int d = 0;
+
+	for (a = 0; s[a] != '\0' && d == 0; a++)
+	{
+		for (b = 0; accept[b] != '\0' && d == 0; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				break;
+			}
+			if (s[a] != accept[b] && accept[b + 1] == '\0')
+				d = 1;
+		}
+	}
+	return (c);
+}

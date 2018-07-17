052e0bbf8a084119ad5c0f5c84e0f3485f54cef3
pmlave <pmlave@gmail.com>
2018-07-17 22:35:55 +0000

    Checking edge case for malloc

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * create_array - allocating memory for an array
+ * @size: Given size of array
+ * @c: Given character with which to initialize array
+ * Return: Pointer to new array
+ */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int a;
@@ -11,5 +17,8 @@ char *create_array(unsigned int size, char c)
 	ar = malloc(size * sizeof(char));
 	for (a = 0; a < size; a++)
 		ar[a] = c;
-	return (ar);
+	if (ar == NULL)
+		return (void *(0));
+	else
+		return (ar);
 }

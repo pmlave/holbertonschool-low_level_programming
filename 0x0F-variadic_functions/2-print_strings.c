e6b4c65648dc1bfd280bd74014589a9537755407
pmlave <pmlave@gmail.com>
2018-07-28 19:25:45 +0000

    Adding parenthesis to nil print

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if ((i + 1 == n || separator == NULL) && string != NULL)
 			printf("%s", string);
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		if (string != NULL && i + 1 != n)
 			printf("%s%s", string, separator);
 	}

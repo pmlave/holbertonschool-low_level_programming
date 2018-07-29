1b66a28fcd78fa71b146703ee24f860013c8ce2f
pmlave <pmlave@gmail.com>
2018-07-29 05:52:40 +0000

    Checking age again

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,10 +13,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else if (!d->name)
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else if (!d->age)
-			printf("Age: 0.000000 \n");
+		printf("Age: %f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else if (!d->owner)

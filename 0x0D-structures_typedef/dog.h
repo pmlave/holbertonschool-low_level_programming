9e602faee3535ca3d90e6dddfcd81af3b539c9d7
pmlave <pmlave@gmail.com>
2018-07-29 01:07:55 +0000

    Updating dog struct for betty style

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - Attributes of dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Description: A structure holding values for aspects of a dog
+ */
 struct dog
 {
 	char *name;

7695fc644e3b6be40417844cf07dff02e910de11
pmlave <pmlave@gmail.com>
2018-07-29 04:39:45 +0000

    Adding new lines for (nil)

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,13 +10,13 @@ void print_dog(struct dog *d)
 	if (d->name)
 		printf("Name: %s\n", d->name);
 	else if (!d->name)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	if (d->age)
 	printf("Age: %f\n", d->age);
 	else if (!d->age)
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
 	else if (!d->owner)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 }

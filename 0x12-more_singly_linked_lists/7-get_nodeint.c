b616fa5c3cf2e55ba97a39d6939be556036e6235
pmlave <pmlave@gmail.com>
2018-08-07 21:38:04 +0000

    Updating NULL options

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int count = 0;
 
-	if (!head)
+	if (!head || !(*head))
 		return (NULL);
 	node = head;
 	while (count < index)

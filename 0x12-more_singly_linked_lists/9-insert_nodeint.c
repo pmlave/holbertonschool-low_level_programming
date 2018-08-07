1250cc7cce9cc245e82601d5434a665adec9fe37
pmlave <pmlave@gmail.com>
2018-08-07 22:28:33 +0000

    Initial test of #9 and updated header file

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserting a node at a numeric index
+ * @head: Pointer to pointer of first node
+ * @idx: Index to insert
+ * @n: Number to add at index
+ * Return: The address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *at;
+	listint_t *prior;
+	unsigned int stop = 0;
+
+	if (!head || !(*head))
+		return (NULL);
+	prior = *head;
+	at = malloc(sizeof(listint_t));
+	if (!at)
+		return (NULL);
+
+	while (stop < idx - 1)
+	{
+		if (!prior)
+			return (NULL);
+		prior = prior->next;
+		stop++;
+	}
+	at->next = prior->next;
+	prior->next = at;
+	at->n = n;
+	return (at);
+}

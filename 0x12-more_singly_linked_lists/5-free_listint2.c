a301516d5ce6f7f74f022bd33f0aa824b877b36f
pmlave <pmlave@gmail.com>
2018-08-07 21:13:22 +0000

    Fixing NULL setting

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *holder;
 
+	if (!head)
+		return;
 	while (*head)
 	{
 		holder = *head;
 		*head = holder->next;
 		free(holder);
 	}
+	*head = NULL;
 }

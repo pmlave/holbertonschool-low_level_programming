2d6367f0deccf9a0337365a982687a4ed9eccf64
pmlave <pmlave@gmail.com>
2018-08-10 01:33:02 +0000

    New solution to #1, updated header file, and updated readme

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * print_binary - print some number in binary form
+ * @n: Given number to print in binary
+ * Return: Void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int pow = 1;
+	int count = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	while (pow <= n)
+	{
+		pow *= 2;
+		count++;
+	}
+	_putchar('1');
+	pow = pow >> 1;
+	n = n - pow;
+
+	while (count > 1)
+	{
+		pow = pow >> 1;
+		if (n >= pow)
+		{
+			_putchar('1');
+			n = n - pow;
+		}
+		else
+			_putchar('0');
+		count--;
+
+	}
+
+}
--- 0x13-bit_manipulation/README.md
+++ 0x13-bit_manipulation/README.md
@@ -1,3 +1,4 @@
 Bitwise operations
 ---
 0. Converting a binary number to an integer
+1. Printing a given integer as a binary number.

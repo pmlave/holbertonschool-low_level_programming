237623c361a83019cc9bb6d29024e0789f5843e2
pmlave <pmlave@gmail.com>
2018-08-31 21:01:20 +0000

    Fixing betty errors

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -9,27 +9,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd = 0;
-        char *buf = NULL;
-        ssize_t char_read = 0;
-        ssize_t char_written = 0;
+	int fd = 0;
+	char *buf = NULL;
+	ssize_t char_read = 0;
+	ssize_t char_written = 0;
 
-        buf = malloc(sizeof(char) * (letters));
+	buf = malloc(sizeof(char) * (letters));
 
-        if (!filename)
-                return (0);
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
-        char_read = read(fd, buf, letters);
-        if (!char_read)
-                return (0);
-        char_written = write(1, buf, char_read);
-        if (char_written == -1)
-                return (0);
-        close(fd);
-        return (char_written);
-
-
-
+	if (!letters)
+		return (0);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	char_read = read(fd, buf, letters);
+	if (!char_read)
+		return (0);
+	char_written = write(STDOUT_FILENO, buf, char_read);
+	if (char_written == -1)
+		return (0);
+	close(fd);
+	return (char_written);
 }

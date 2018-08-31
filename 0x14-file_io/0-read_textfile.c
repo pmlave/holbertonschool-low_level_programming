a47e9f7a2542d8176fceb10af10fc54028029e73
pmlave <pmlave@gmail.com>
2018-08-31 20:56:33 +0000

    First solution to #0

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reading and writing from a file
+ * @filename: The file to be read
+ * @letters: The number of characters that should be read
+ * Return: Number of characters printed or 0 for failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        int fd = 0;
+        char *buf = NULL;
+        ssize_t char_read = 0;
+        ssize_t char_written = 0;
+
+        buf = malloc(sizeof(char) * (letters));
+
+        if (!filename)
+                return (0);
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+                return (0);
+        char_read = read(fd, buf, letters);
+        if (!char_read)
+                return (0);
+        char_written = write(1, buf, char_read);
+        if (char_written == -1)
+                return (0);
+        close(fd);
+        return (char_written);
+
+
+
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,3 @@
+Working with file I/O using system calls
+---
+0. Open, read, write, and close a file using system calls.

eb0bf6f388fba0f43a2c1365bdcb5f2e2543743a
pmlave <pmlave@gmail.com>
2018-09-15 22:28:55 +0000

    Updated header file and solution to #5

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnotdeint_at_index - get a node at a certain index
+ * @head: Pointer to head node
+ * @index: Index from which to pull node
+ * Return: Node or NULL if DNE
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int node_number = 0;
+
+	if (!head)
+		return (NULL);
+	while (current)
+	{
+		if (node_number == index)
+			return (current);
+		current = current->next;
+		node_number++;
+	}
+	return (NULL);
+}

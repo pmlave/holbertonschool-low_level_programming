c9781a9945294e0bbe693e1c12f22d79df753fe5
pmlave <pmlave@gmail.com>
2018-09-15 19:52:35 +0000

    Fixing betty style

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,16 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - printing a doubly linked list
+ * @h: Head pointer
+ * Return: Count of number of elements in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *pointing = h;
 	size_t count = 0;
+
+	if (!h)
+		return (NULL);
 
 	while (pointing)
 	{

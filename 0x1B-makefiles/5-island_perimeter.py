1f116eb32d73026eed020808fac1f5fd185a17ea
Paul Lave <pmlave@gmail.com>
2018-11-17 00:36:07 +0000

    New solution to #5

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Python module containing island_perimeter function'''
+def island_perimeter(grid):
+    '''Function to find the perimeter of an 'island' in a list of lists'''
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            else:
+                if grid[i+1][j] == 0:
+                    perimeter += 1
+                if grid[i-1][j] == 0:
+                    perimeter += 1
+                if grid[i][j+1] == 0:
+                    perimeter += 1
+                if grid[i][j-1] == 0:
+                    perimeter += 1
+    return(perimeter)

f18606edc87a9a54be0d07b4f28dfd061b497e8e
Paul Lave <pmlave@gmail.com>
2018-11-17 01:38:13 +0000

    Final solution to #5, fixed pep8 issues

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -10,9 +10,9 @@ def island_perimeter(grid):
             if grid[i][j] == 0:
                 continue
             else:
-                if  i == len(grid) - 1 or grid[i+1][j] == 0:
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
                     perimeter += 1
-                if  i == 0 or grid[i-1][j] == 0:
+                if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
                 if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
                     perimeter += 1

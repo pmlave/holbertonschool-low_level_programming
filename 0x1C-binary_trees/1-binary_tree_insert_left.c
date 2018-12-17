8ba9a3989deb0c522cc65dfbdf2632d1de85c5c3
Paul Lave <pmlave@gmail.com>
2018-12-17 01:49:06 +0000

    Updated edge case check on #1 and included updated header

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child;
 
+	if (!parent)
+		return (NULL);
 	child = malloc(sizeof(binary_tree_t));
 	if (!child)
 		return (NULL);

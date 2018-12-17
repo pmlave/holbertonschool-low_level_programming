6f4d4b8a35550dee9ae6a047a55258eb707e3240
Paul Lave <pmlave@gmail.com>
2018-12-17 02:54:02 +0000

    Added extra edge case check for #6

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -9,7 +9,9 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (!func)
+		return;
+	if (!tree)
 		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);

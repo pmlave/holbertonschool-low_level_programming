e0d3a7b0aa772b3c5dd5133267d59ab2ec9a3efc
Paul Lave <pmlave@gmail.com>
2018-12-17 01:10:09 +0000

    Fixing betty style on header and #0

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,16 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creating a new node for the binary tree
+ * @parent: Parent node pointer
+ * @value: Value to put into struct
+ * Return: Pointer to child node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *child;
-    child = malloc(sizeof(binary_tree_t));
-    if (!child)
-    {
-        return (NULL);
-    }
-    child->parent = parent;
-    child->n = value;
-    child->left = NULL;
-    child->right = NULL;
-    return (child);
+	binary_tree_t *child;
+
+	child = malloc(sizeof(binary_tree_t));
+	if (!child)
+	{
+		return (NULL);
+	}
+	child->parent = parent;
+	child->n = value;
+	child->left = NULL;
+	child->right = NULL;
+	return (child);
 }

7e4669441e8e357ab3c46b45fa0e600f17827d99
Paul Lave <pmlave@gmail.com>
2018-12-17 23:29:25 +0000

    Updated #13 for betty style

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - count the number of leaves on a binary tree
+ * binary_tree_nodes - count the number of nodes on a binary tree
  * @tree: Pointer to the root node
  * Return: The number of leaves
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        if (!tree->left && !tree->right)
-                return (0);
-        return (binary_tree_nodes(tree->left)
-                + binary_tree_nodes(tree->right) + 1);
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	return (binary_tree_nodes(tree->left)
+		+ binary_tree_nodes(tree->right) + 1);
 
 }

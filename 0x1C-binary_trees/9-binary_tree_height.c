375bf8176a019cbfbdcd9a1d3ff41e30813a2856
Paul Lave <pmlave@gmail.com>
2018-12-18 00:38:01 +0000

    Reformatted code to account for an error

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -7,23 +7,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	int i = 0, j = 0;
 
 	if (!tree)
 		return (0);
-	while (tree->left || tree->right)
-	{
-		if (tree->left)
-		{
-			tree = tree->left;
-			count++;
-		}
-		else
-		{
-			tree = tree->right;
-			count++;
-		}
-	}
-	return (count);
-
+	if (!tree->left && !tree->right)
+		return (0);
+	i = binary_tree_height(tree->left) + 1;
+	j = binary_tree_height(tree->right) + 1;
+	if (j > i)
+		return (j);
+	return (i);
 }

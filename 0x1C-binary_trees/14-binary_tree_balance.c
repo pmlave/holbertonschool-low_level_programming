e891c4febdf4fb66dee8353130fe1c0e6bcb6327
Paul Lave <pmlave@gmail.com>
2018-12-18 01:01:36 +0000

    Updated solution to #14

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -13,3 +13,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (binary_tree_height(tree->left)
 		- binary_tree_height(tree->right));
 }
+
+
+/**
+ * binary_tree_height - calculate height of the binary tree
+ * @tree: Pointer to root node
+ * Return: Height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int i = 0, j = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	i = binary_tree_height(tree->left) + 1;
+	j = binary_tree_height(tree->right) + 1;
+	if (j > i)
+		return (j);
+	return (i);
+}

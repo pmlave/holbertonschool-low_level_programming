0f2374cb1356fd89903ebbbe1287f43e2c8a3fbc
Paul Lave <pmlave@gmail.com>
2018-12-22 05:14:18 +0000

    Updated solution for #0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,10 +9,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i = 0;
 
-	new_table = malloc((size * 8) + 16);
+	if (size == 0)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
+
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new_table->array[i] = NULL;
+		i++;
+	}
+
 	return (new_table);
 
 

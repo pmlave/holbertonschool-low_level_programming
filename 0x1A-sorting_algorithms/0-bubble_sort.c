557b03f4192a0f8ac0fbb289a9831075b13f80c5
Paul Lave <pmlave@gmail.com>
2018-12-23 00:17:01 +0000

    Initial commit containing a solution to #0, a new readme, a new header, and the complexity answer to #0 as well

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - implement the bubble sort algorithm
+ * @array: Given array to sort
+ * @size: Size of the given array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int temp = 0;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array((const int *)array, size);
+			}
+		}
+	}
+}
---
+++ 0x1A-sorting_algorithms/README.md
@@ -0,0 +1,3 @@
+In this project, we are looking into time complexity and sorting algorithms.
+---
+0. Implement bubble sort.
